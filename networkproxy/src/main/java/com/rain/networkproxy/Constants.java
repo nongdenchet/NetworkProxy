d044cf54ae47438dacd80394a5bd6285a2849f52
Quan Vu <nongdenchet8435848@gmail.com>
2018-12-23 18:40:29 +0800

    [REFACTOR] Refactor receiver

--- networkproxy/src/main/java/com/rain/networkproxy/Constants.java
+++ networkproxy/src/main/java/com/rain/networkproxy/Constants.java
@@ -2,7 +2,7 @@ package com.rain.networkproxy;
 
 public final class Constants {
     public static final String TAG = "NetworkProxy";
-    public static final String INSTRUCTION_EVENT = "NP_EVENT_INSTRUCTION";
+    public static final String INSTRUCTION_EVENT = "com.rain.networkproxy.INSTRUCTION";
     public static final String INSTRUCTION_EVENT_DATA = "data";
     public static final String INSTRUCTION_EVENT_BODY = "body";
 
