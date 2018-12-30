7d6d04fa1ab10599436abe759dde46fa7acca754
Quan Vu <nongdenchet8435848@gmail.com>
2018-12-30 12:38:49 +0800

    [CONFIG] Upload to bintrav

--- networkproxy-no-op/src/main/java/com/rain/networkproxy/NetworkProxy.java
+++ networkproxy-no-op/src/main/java/com/rain/networkproxy/NetworkProxy.java
@@ -1,12 +1,12 @@
 package com.rain.networkproxy;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
 import okhttp3.Interceptor;
 import okhttp3.Response;
 
 import java.io.IOException;
 
+@SuppressWarnings({"NullableProblems", "unused"})
 public final class NetworkProxy {
     private static NetworkProxy instance;
 
@@ -14,39 +14,16 @@ public final class NetworkProxy {
         // No-op
     }
 
-    @SuppressWarnings("unused")
     public static void init(Context context) {
-        instance().initialize();
-    }
-
-    public static Interceptor interceptor() {
-        return instance().getInterceptor();
-    }
-
-    private void initialize() {
         // No-op
     }
 
-    private Interceptor getInterceptor() {
+    public static Interceptor interceptor() {
         return new Interceptor() {
-            @NonNull
             @Override
-            public Response intercept(@NonNull Chain chain) throws IOException {
+            public Response intercept(Chain chain) throws IOException {
                 return chain.proceed(chain.request());
             }
         };
-    }
-
-    @NonNull
-    private static NetworkProxy instance() {
-        if (instance == null) {
-            synchronized (NetworkProxy.class) {
-                if (instance == null) {
-                    instance = new NetworkProxy();
-                }
-            }
-        }
-
-        return instance;
     }
 }
