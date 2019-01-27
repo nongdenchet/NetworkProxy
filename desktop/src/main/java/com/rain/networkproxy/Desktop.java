6d88075c9610dd7e10761df18cd87ffcdcc99812
Quan Vu <nongdenchet8435848@gmail.com>
2019-01-27 15:10:02 +0800

    (CONFIG) Init JavaFX desktop app

--- desktop/src/main/java/com/rain/networkproxy/Desktop.java
+++ desktop/src/main/java/com/rain/networkproxy/Desktop.java
@@ -1,42 +1,20 @@
 package com.rain.networkproxy;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.Socket;
-import java.util.Scanner;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
-public class Desktop {
+public class Desktop extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(new GridPane(), 300, 275));
+        primaryStage.show();
+    }
 
     public static void main(String[] args) {
-        final Scanner scanner = new Scanner(System.in);
-        try (Socket socket = new Socket("127.0.0.1", 8000)) {
-            System.out.println("Server connected");
-
-            final DataInputStream inputStream = new DataInputStream(socket.getInputStream());
-            final DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
-            dataOutputStream.writeUTF("start");
-
-            while (!socket.isClosed()) {
-                System.out.println("Waiting for message");
-
-                final String message = inputStream.readUTF();
-                System.out.println("Message: " + message);
-
-                if (message.equals("[]")) {
-                    System.out.println("No pending response --> Skipping input");
-                } else {
-                    System.out.println("Your input:");
-                    dataOutputStream.writeUTF(scanner.nextLine().trim());
-                }
-            }
-
-            inputStream.close();
-            dataOutputStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            System.out.println("Connection closed");
-        }
+        launch(args);
     }
 }
